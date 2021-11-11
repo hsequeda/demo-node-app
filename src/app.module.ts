d71f964c21c7e731ccb503f6d21c85913489dd2b
Henry <henrysg88@gmail.com>
2021-11-11 14:39:26 -0500

    feat(General): Import dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FamilyModule } from './family/family.module';
+import { AccessModule } from './access/access.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    FamilyModule,
+    AccessModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT),
+      entities: [
+        join(__dirname, '**', 'adapter/*.entity.{ts,js}'),
+        join(__dirname, '**', '*.entity.{ts,js}'),
+      ],
+      synchronize: false,
+    }),
+  ],
 })
 export class AppModule {}
